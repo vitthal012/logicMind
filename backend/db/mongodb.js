5373a803011e0d1db9150c8318da656e35ad0988
vitthal012~ <srivastava.vitthal01@gmail.com>
2026-03-26 21:16:39 +0530

    added reveal button and timer-1

--- backend/db/mongodb.js
+++ backend/db/mongodb.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("creating client");
 const client = new MongoClient(process.env.MONGO_URL, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -11,4 +12,25 @@ const client = new MongoClient(process.env.MONGO_URL, {
   }
 });
 
-export default client;
+let db;
+
+async function connectclus(){
+  try{
+    console.log("Starting...")
+
+    await client.connect();//cluster connected
+    console.log("cluster connected")
+
+    db=client.db("logicmind");//database connected
+    console.log("database connected");
+  }
+  catch(e){
+    console.log(e.message);
+  }
+}
+
+
+
+export {connectclus,db};
+
+
